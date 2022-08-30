337f18e133a0b823add0d75399301fa261f7cb96
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-08-30 18:07:42 +0300

    Time: 8421 ms (5.01%), Space: 13.8 MB (99.70%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        nums.append(0)
+        for i in range(1,len(nums)):
+
+            lef = (sum(nums[0:i-1]))
+            rig = (sum(nums[i:]))
+            if (lef == rig) : return i-1
+        return -1
+        
