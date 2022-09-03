66c9a94f27ae7e25bd9e15131430cf74671b5cd0
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-09-03 15:08:35 +0300

    Time: 19 ms (93.57%), Space: 13.6 MB (53.71%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        nums1 = int(num1)
+        nums2 = int(num2)
+        return str(nums1 * nums2)
+        
