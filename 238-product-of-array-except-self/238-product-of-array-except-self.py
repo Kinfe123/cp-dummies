d4a92df6f0429edaf6d258dbb7b295789481577c
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-09-17 21:02:51 +0300

    Time: 509 ms (18.53%), Space: 21.4 MB (58.51%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,38 +1,18 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        n = len(nums)
-        if(n == 1):
-            print(0)
-            return
- 
-        # Allocate memory for temporary arrays left[] and right[]
-        left = [0]*n
-        right = [0]*n
 
+class Solution:
     
-        prod = [0]*n
-
+    def productExceptSelf(self, arr: List[int]) -> List[int]:
         
-        left[0] = 1
-
+        prefix = 1
+        postfix = 1
+        output = [1] * len(arr)
+        for i in range(len(arr)):
+            output[i] = prefix
+            prefix*=arr[i]
+        for i in range(len(arr)-1 , -1 , -1 ):
+            output[i] *= postfix
+            postfix *= arr[i]
+        return output 
+            
         
-        right[n - 1] = 1
-
-        # Construct the left array
-        for i in range(1, n):
-            left[i] = nums[i - 1] * left[i - 1]
-
-        # Construct the right array
-        for j in range(n-2, -1, -1):
-            right[j] = nums[j + 1] * right[j + 1]
-
-        for i in range(n):
-            prod[i] = left[i] * right[i]
-
         
-        return prod
-
