6d7ad07459b1f399da2ce97d4e14fb8be8c3ef18
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-09-23 21:50:26 +0300

    Time: 162 ms (25.08%), Space: 14.9 MB (33.04%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -4,16 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        if n == 0:
-            return 0
-        slow = 0
-        fast = 1
-        while fast < n:
-            if nums[fast]!=nums[slow]:
-                slow+=1
-                nums[slow] = nums[fast]
-            fast+=1
-        return slow+1
-
-        
+        left = 1
+        for right in range(1 , len(nums)):
+            if nums[right] != nums[right-1]:
+                nums[left] = nums[right]
+                left+=1
+                
+        return left
