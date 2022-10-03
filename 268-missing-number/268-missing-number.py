793a694554327966d50f55e9502ec35381da187b
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-10-03 17:16:33 +0300

    Time: 281 ms (44.59%), Space: 14.9 MB (99.67%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+            else:
+                pass
+        return len(nums)
+        
