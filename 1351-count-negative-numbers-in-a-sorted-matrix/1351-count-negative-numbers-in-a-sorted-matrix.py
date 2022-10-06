d1fc45f012df95fe6fa1987999188cd94d6d23b3
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-10-06 09:22:10 +0300

    Time: 251 ms (41.68%), Space: 15 MB (39.90%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -1,17 +1,17 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        #We can do it with a bruteforce approach - O(n^2) 
-        #we can iterate each element in nested or 2d array
-        #Bruteforce approach -
-        #Time Comp = O(n^2)  and Space Comp = O(1) - constant space
-        count = 0
         
-        for i in grid:#traverse through the outer list
-            for j in i:#For each element in the nested list
-                if j < 0:
-                    count+=1#keep counting when we get the number less than 0
+        
+        i = len(grid) -1 
+        j = 0
+        count = 0
+        x , y = 0 , grid[0]
+        while i >= x and j < len(y):
+            if grid[i][j] < 0:
+                count += len(y) - j
+                i-=1 # we look for others
+            else:
+                j+=1
         return count
-        #The second approach we can use two pointer concept 
-       
     
         
