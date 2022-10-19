1abee3b1e00a7b555b8ce90ffeaf59813fcdaff2
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-10-19 22:11:47 +0300

    Time: 2152 ms (29.86%), Space: 18.8 MB (99.85%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,13 +1,26 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        
+        # countforsub  , l  , oddc , answer = 0  , 0 , 0 , 0
+        # for r in range(len(nums)):
+        #     if nums[r] % 2:
+        #         oddc+=1
+        #         countforsub=0
+        #     while oddc == k:
+        #         if nums[l] % 2:
+        #             oddc-=1
+        #             l+=1
+        #         countforsub+=1
+        #     answer+=countforsub
+        # return answer
         count = 0
         curr_sum = 0
         map_ = {}
         for i in range(len(nums)):
-            if nums[i] % 2 == 0:
-                nums[i] = 0
-            else:
+            if nums[i]%2:
                 nums[i] = 1
+            else:
+                nums[i] = 0
         for i in range(len(nums)):
             curr_sum+=nums[i]
             if curr_sum == k:
@@ -18,6 +31,6 @@ class Solution:
                 map_[curr_sum] += 1
             else:
                 map_[curr_sum] = 1
-        return count 
+        return count
 
                 
