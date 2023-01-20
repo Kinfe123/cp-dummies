eeb194bf62f8e19378e3e287fc26b3671f17a95f
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-01-20 08:06:26 +0300

    Time: 272 ms (47.20%), Space: 14 MB (8.17%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,20 +1,20 @@
-class Solution(object):
-    def smallerNumbersThanCurrent(self, nums):
-        counter = 0
-        arr = []
-        for i in range(len(nums)):
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        lists = nums.copy()
+        nums.sort()
+        map_ = {}
+        result = []
+        for i in range(len(nums)-1 , -1 , -1):
             counter = 0
-            for j in nums:
-                if nums[i]>j:
+            for j in range(i):
+                if nums[i] > nums[j]:
                     counter+=1
+            result.append(counter)
+            map_[nums[i]] = counter
+        for i in range(len(lists)):
+            for keys , values in map_.items():
+                if keys == lists[i]:
+                    result[i] = values
                     
-            arr.append(counter)
+        return result 
                     
-        return arr   
-    
-solution = Solution()
-nums = [8,1,2,2,3]
-print(solution.smallerNumbersThanCurrent(nums))
-
-            
-        
