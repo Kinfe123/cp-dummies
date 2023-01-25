2b6c65df5d2c40368cde35d7db552c142d40bcfe
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-01-25 18:21:37 +0300

    Time: 684 ms (35.09%), Space: 21 MB (25.88%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,20 +1,31 @@
-class Solution:
+
+class Solution:           
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        left , right = 0 , len(people) -1 
+        counter = 0
+        l , r = 0 , len(people)-1
         people.sort()
-        count = 0
-        while left <= right:
-            if people[left] + people[right] <= limit:
-                left+=1
-                right-=1
-            
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                r-=1
+                l+=1
+                #Both can taken since there weight is at most to the limit
+              
             else:
-                right-=1
-            count+=1
-        return count
+                r-=1
+                #It has to go alone since it is way to heavier for being taken by
+                
+                
+            
+            counter+=1
+        return counter
+        
+        
         
                 
-                
-                
-            
+
+        
+       
+        
+        
+        
         
