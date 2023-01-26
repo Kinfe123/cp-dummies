1c4e2f1171bcf449af75e1db3345216e8419d259
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-01-26 09:45:09 +0300

    Time: 720 ms (57.23%), Space: 26.4 MB (70.85%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,38 +1,20 @@
-class Solution(object):
-    def maxOperations(self, nums, k):
-      
-    
-    # Sort array in increasing order
-        nums = sorted(nums)
-
-        # Stores the final result
-        result = 0
-
-        # Initialize the left and right pointers
-        start, end = 0, len(nums) - 1
-
-        # Traverse array until start < end
-        while (start < end):
-            if (nums[start] + nums[end] > k):
-
-                # Decrement right by 1
-                end -= 1
-
-            elif (nums[start] + nums[end] < k):
-
-                # Increment left by 1
-                start += 1
-
-            # Increment result and left
-            # pointer by 1 and decrement
-            # right pointer by 1
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        counter = 0
+        low , high = 0 , len(nums)-1
+        while low < high:
+            if nums[low] + nums[high] > k:
+                high-=1
+            elif nums[low] + nums[high] < k:
+                low+=1
             else:
-                start += 1
-                end -= 1
-                result += 1
-
-                    
-        return result
-                    
-                    
+                counter+=1
+                low+=1
+                high-=1
+                
+        return counter 
+            
+        
+        
         
