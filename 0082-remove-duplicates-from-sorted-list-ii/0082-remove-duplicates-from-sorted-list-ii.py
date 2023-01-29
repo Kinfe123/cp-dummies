1dddbdbae8717499c70bbe52c3a848e7ec24a005
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-01-29 19:48:46 +0300

    Time: 44 ms (70.02%), Space: 14 MB (24.68%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -8,20 +8,25 @@ class Solution:
         
         
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        unique_l = []
-        temp = head
-        while temp:
-            unique_l.append(temp.val)
-            temp = temp.next
-        c = Counter(unique_l)
-        unique_l = [k for k, v in c.items() if v == 1]
-      
-        
-        dummy = current = ListNode()
-        for i in unique_l:
-            current.next = ListNode(i)
-            current = current.next
+        dummy = curr = ListNode()
+        dummy.next = head
+        while head:
+            if head.next and head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                    #Deleting the top of the head 
+                #when it goes out of the loop we should not have to include the one that 
+                #has a duplicate and move it to the next and attach the rest to curr -
+                head = head.next
+                curr.next = head
+            else:
+                curr = curr.next
+                
+                head = head.next
         return dummy.next
+                
+
+        
             
         
         
