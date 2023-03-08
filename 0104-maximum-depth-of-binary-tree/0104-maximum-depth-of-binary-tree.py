896b8e163162ef209d45113cf0e85b6493075d38
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-03-08 22:58:28 +0300

    Time: 45 ms (53.35%), Space: 16.2 MB (50.96%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -12,8 +12,29 @@ class Solution:
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         #iterative BFS 
         '''
+
         we used queue since we want to go level by level to access every level
         which have atmost two children 
         
+        
+        
+        
         '''
+        if root is None:
+            return 0
+        result = 1
+        stack = [(root , result)]
+        while stack:
+            popped , depth = stack.pop()
+            result = max(result , depth)
+            if popped.right:
+                stack.append((popped.right , depth+1))
+            if popped.left:
+                stack.append((popped.left , depth+1))
+        return result
+    
+                
+            
+            
+            
         
