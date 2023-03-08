c6a0320c4c5cb854c0cbcf04bade9f919dc958f0
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-03-08 22:42:15 +0300

    Time: 44 ms (59.49%), Space: 16.3 MB (50.96%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -7,29 +7,13 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         #recursive dSF
-        # if root is None:
-        #     return 0
-        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        if root is None:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         #iterative BFS 
         '''
         we used queue since we want to go level by level to access every level
         which have atmost two children 
         
         '''
-        if root is None:
-            return 0
-        queue = deque([root])
-        depth = 0
-        while queue:
-            for i in range(len(queue)):
-                popped = queue.popleft()
-                if popped.left:
-                    queue.append(popped.left)
-                if popped.right:
-                    queue.append(popped.right)
-            depth+=1
-        return depth 
-                
-                
-                
-            
+        
