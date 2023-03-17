b23b968c0c232128d235b4e7b9c25066dec5bc2a
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-03-17 07:47:34 +0300

    Time: 599 ms (82.40%), Space: 27.3 MB (78.83%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,4 +1,17 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        return bisect.bisect_left(arr[:arr.index(max(arr))] , max(arr))
+        low = 0
+        high = len(arr)-1
+        while low <= high:
+            mid = low+ (high-low)//2
+            if arr[mid-1] < arr[mid] > arr[mid+1]:
+                return mid
+            elif arr[mid] < arr[mid-1]:
+                high = mid
+            else:
+                low = mid+1
+                
+                
+            
+        
         
