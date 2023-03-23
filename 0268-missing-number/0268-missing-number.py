63fd2e49297fc56bd4b02736353d0b77882d6c06
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-03-23 18:54:27 +0300

    Time: 125 ms (95.34%), Space: 15.2 MB (67.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.append('x')
-        for i in range(len(nums)):
-            while nums[i] != 'x' and nums[i] != i:
-                temp = nums[i]
-                nums[i] , nums[temp] = nums[temp] , nums[i]
-        for i in range(len(nums)):
-            if nums[i] == "x":
-                return i
-            
+        return (len(nums) * (len(nums) + 1)) // 2 - sum(nums)  
                 
             
         
