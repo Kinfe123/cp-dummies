30815b85a7498589ea1211f9443681b5bc88194e
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-03-24 18:30:58 +0300

    Time: 57 ms (44.85%), Space: 14.2 MB (6.87%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        result = []
+        main_diag = set()
+        post_main_diag = set()
+        columns_ = set()
+        chess_ = [["."] * n for i in range(n)]
+        sets_ = set()
+        # above all are the case where will be under attack 
+        def findWay(each_row):
+            if each_row == n:
+                string = ["".join(each) for each in chess_]
+                result.append(string[:])
+                stringoch = "".join(string)
+                sets_.add(stringoch)
+                return 
+            
+            for each_column in range(n):
+                if each_column - each_row in main_diag or each_column + each_row in post_main_diag or each_column in columns_:
+                    #to avoid attacking of the queens
+                    continue
+                columns_.add(each_column)
+                main_diag.add(each_column - each_row)
+                post_main_diag.add(each_column + each_row)
+                chess_[each_row][each_column] = "Q"
+                #after doing this we have to lookup for any other possiblities 
+                findWay(each_row+1)
+                #we will be finding any possible way until we are out of rows
+                
+                
+                #Next we will be doing some clean up to restore back to 
+                #previous states
+                columns_.remove(each_column)
+                main_diag.remove(each_column - each_row)
+                post_main_diag.remove(each_column + each_row)
+                chess_[each_row][each_column] = "."
+        findWay(0)
+        return len(sets_) 
+                
+                
+        
