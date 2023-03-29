4657596132104b262c28fdff67cf86f1663384cf
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-03-29 22:12:31 +0300

    Time: 43 ms (52.15%), Space: 14.1 MB (16.93%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,25 +1,31 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-     
-        def backtrackish(nums, path=[]):
+        result , current = [] , []
+        # state_ = 0
+        def backtrackish(index , state_):
+            if index == len(nums):
+                result.append(current.copy())
+                return 
             
-        
-            if not nums: 
-                result.append(path) 
-                #till we have left with no elements left  
-
-            for i in range(len(nums)): 
-       
-                
-               
-                backtrackish(nums[:i] + nums[i+1:], path+[nums[i]]) 
-                
-                
-       
-        
-        backtrackish(nums , path=[])
+            for i in range(len(nums)):
+                # if it is not been permutated 
+                if state_ & (1 << i) == 0:
+                    #we will include to  be permutated 
+                    #by setting it on for the next iter
+                    state_ |= (1 << i)
+                    current.append(nums[i] )
+                  
+                    backtrackish(index+1 , state_)  
+                    
+                    
+                    current.pop()
+                    # we will do some clean up and finding another way to permutate the numbers
+                    state_ ^= (1 << i)
+                    
+        backtrackish(0 , 0)
         return result
-                
             
-        
+            
+            
+            
+            
