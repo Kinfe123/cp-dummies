be5fc6ffca1526a6186db8c0fc8422b091389096
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-03-30 22:31:32 +0300

    Time: 217 ms (41.65%), Space: 16.1 MB (12.82%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,22 +1,29 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        lists  = [i for i in range(1 , len(nums)+1)]
         result = []
-        counter = Counter(nums)
-        result.append(list(set(lists).difference(set(nums))))
-        result.append(list(keys for keys , values in counter.items() if values == 2))
-    
-        return [result[1][0] , result[0][0]]
-#         nums.sort()
-#         result = [0] * 2
-#         i = 0
-#         for i in range(len(nums)):
-#             if nums[i] ^ lists[i]:
+        lists = [i for i in range(1 ,len(nums) + 1)]
+        n = len(nums)
+        nums.sort()
+
+        duplicated_ = 0
+        #duplication handling phase
+        for i in range(1, len(nums)):
+            if nums[i] ^ nums[i-1] == 0:
+                duplicated_ = nums[i]
+                break
+        
                 
-#                 result[i]  = nums[i] ^ lists[i]
-#                 i+=1
+        #Missing number finding phase
+        nums = list(set(nums))
+        total = len(nums)+1 
+        missed = 0
+      
+        for i in range(len(nums)):
+            total ^= nums[i]
+            missed^= i
+        missed ^= len(nums)
+        missed = total ^ missed
+        #merging the missing and duplicates together  
+        return [duplicated_ , missed] 
             
-                
-#         return result 
-                
-                
+        
