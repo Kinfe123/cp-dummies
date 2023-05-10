6b5b646c8537ae7a65f405c20b12d438d3f0f367
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-05-10 19:50:12 +0300

    Time: 683 ms (49.62%), Space: 23.5 MB (26.28%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -3,19 +3,23 @@ class Solution:
         coloring = [0 for _ in range(len(graph))]
         order = []
         def top_sort(node):
+            #Cycle has been detected and we can not move anyfar.
             if coloring[node] == 1:
                 return False
             
             coloring[node] = 1
             for n in graph[node]:
                 if coloring[n] == 2:
-                #Cycle has been detected
+                    #Processed before 
                     continue
                 if not top_sort(n):
                     return False
             
             coloring[node] = 2
+            #Marking it as processed since it wil be leading us to terminal node 
+        
             order.append(node)
+            
             return True
         for i in range(len(graph)):
             if coloring[i] == 0:
