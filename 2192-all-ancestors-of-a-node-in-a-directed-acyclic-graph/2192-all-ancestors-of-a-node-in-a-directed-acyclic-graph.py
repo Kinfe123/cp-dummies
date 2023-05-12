1a81cb458d455183554c5092e84fcaf8956c27c4
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-05-12 10:36:47 +0300

    Time: 767 ms (85.96%), Space: 48.3 MB (29.23%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,41 +1,35 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        result = []
+        result = [set() for _ in range(n)]
         graph = defaultdict(list)
         glo_visited = set()
+        indegree = [0 for _ in range(n)]
         for f , to in edges:
-            graph[to].append(f)
-    
+            indegree[to]+=1
             
-        def dfs(node , res):
-            visited.add(node)
-            
-            
-            for each in graph[node]:
-               
-                if each not in visited:
-                    
-                    dfs(each , res)
-            
-     
-            #down the path we should not have to get in to our parents
-            
-            # res.sort()
-            # if res not in result:
-            #     result.append(res)
-            # return 
-            
-        for i in range(n):
-            if not len(graph[i]):
-                result.append([])
-            else:
-                visited = set()
-                
-                dfs(i , [])
-                #since i should have to take all of it ansestor, we have to remove the current one 
-                visited.remove(i)
-                result.append(sorted(set(visited)))
-                
-        return result
-                    
+            graph[f].append(to)
         
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        while q:
+            curr = q.popleft()
+            # print('the curr: ' , curr)
+            #the logic goes here
+            
+            for nei in graph[curr]:
+                # result[nei].add(curr)
+            
+                result[nei].update(result[curr], [curr])
+                indegree[nei]-=1
+                if indegree[nei] == 0:
+                    # result[curr] = sorted(list(result[curr]))
+                    q.append(nei)
+          
+        result = [sorted(list(i)) for i in result]
+    
+        return result
+            
+            
