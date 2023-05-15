c557d2d37195f55bdf46b88f9f8ea572f2af6dcd
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-05-15 10:40:18 +0300

    Time: 716 ms (78.85%), Space: 20.1 MB (14.89%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,16 +1,34 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        matrices = [[0] * numCourses for _ in range(numCourses)]
-        #for making sure of the transitive property just using by the intermiediate vertext from the range(N)
-        for u , v in prerequisites:
-            matrices[u][v] = 1
-        for inter in range(numCourses):
-            for row in range(numCourses):
-                for col in range(numCourses):
-                    inter_ = matrices[row][inter] and matrices[inter][col]
-                    matrices[row][col] = matrices[row][col] or (inter_)
+        map_ = defaultdict(list)
+        process = defaultdict(set)
+        indeg = [0] * numCourses
+        for pre , course in prerequisites:
+            map_[pre].append(course)
+            indeg[course] += 1
+            
+        q = deque()
+        for i in range(numCourses):
+            if not indeg[i]:
+                q.append(i)
+                
+        
+        
+        while q:
+            curr = q.popleft()
+            #we have found the one who has not any kind of prerequistes so lets find out for whom it can be a prerequisites
+            for nei in map_[curr]:
+                #here we will be doing somekind of transitivity
+                process[nei].add(curr)
+                process[nei].update(process[curr])
+                indeg[nei]-=1
+                if not indeg[nei]:
+                    q.append(nei)
                     
-        return [matrices[a][b] for a , b in queries]
+        return [u in process[v] for u , v in queries]
+
+                    
+        
                     
                     
                     
