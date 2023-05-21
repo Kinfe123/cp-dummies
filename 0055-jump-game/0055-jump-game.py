3752de1340ac31b323bf9dd807e5fecefe0957e3
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-05-21 11:58:14 +0300

    Time: 4792 ms (10.40%), Space: 51.7 MB (5.06%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -7,22 +7,22 @@ class Solution:
         def dfs(index):
             nonlocal res 
             if index >= len(nums)-1:
-                res = True
+                
                 return True
             next_ = index+1
             visited.add(index)
             for node_ in range(next_ , min(len(nums) , index+nums[index] + 1)):
                 if node_ not in visited and dfs(node_):
-                    res = True 
+                   
                     return True 
             
         
             
                     
             
+            return False
             
+        return dfs(0)
             
-        dfs(0)
-            
-        return res
+        # return res
        
