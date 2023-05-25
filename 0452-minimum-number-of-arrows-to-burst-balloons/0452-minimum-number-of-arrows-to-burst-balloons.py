56347fd0d505ef047992f1bedde11af1c7c413a0
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-05-25 17:22:33 +0300

    Time: 1336 ms (55.22%), Space: 62.7 MB (28.26%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[0])
+        min_= 1
+        # the one with the first would be the one who get brusted , then we will be brust
+        end = points[0][1]
+        for i in range(1 , len(points)):
+            start_ , end_ = points[i]
+            if start_ > end:
+                min_+=1
+                end = end_
+            else:
+                end = min(end , end_)
+                
+                
+            
+     
+        return min_
