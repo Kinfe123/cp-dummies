8c638bcb18d9e6509ec110f9e7c894f1314d8034
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-05-31 19:08:11 +0300

    Time: 1373 ms (66.67%), Space: 16.7 MB (52.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,31 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        count = 0
-        res = []
-        if amount == 0:
-            return 0
-        if amount in set(coins):
-            return 1
-        
-        
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = [0]
-        @lru_cache(None)
-        def collect(x):
-            if x == 0:
-                return x
-            #for other paths
-            res = float('inf')
-            for i in range(len(coins)):
-                if x >= coins[i]:
-                    
-                    res = min(res , 1+collect(x-coins[i]))
-            return res
-        
-        val = collect(amount) 
-        if val == float('inf'):
+        dp = [float('inf')] * (10**4+1)
+        dp[0] = 0
+        for i in range(1 , amount+1):
+            for coin in coins:
+                if i-coin>=0:
+                    dp[i] = min(dp[i] , dp[i - coin] + 1) 
+        # print(dp)
+        if dp[amount] == float('inf'):
             return -1
+        return dp[amount]
         
-        return val
-            
-         
