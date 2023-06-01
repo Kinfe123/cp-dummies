8c26c2b95646fb88caf349950fec0f47fb0dc37b
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-06-01 19:09:14 +0300

    Time: 1602 ms (5.87%), Space: 243.6 MB (14.51%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,11 +2,12 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         
         dp = defaultdict(int)
-        @cache
+        
         def transact(indx , can):
             if indx == len(prices):
                 return 0
-            
+            if (indx , can) in dp:
+                return dp[(indx ,can)]            
             
             
             op1 = transact(indx + 1 , can)
@@ -14,9 +15,9 @@ class Solution:
             if can:
                 op2 = -prices[indx] + transact(indx+1 , False)
             else:
-                op2 = prices[indx] - fee + transact(indx+1 , True)
-
-            return max(op1, op2)
+                op2 = prices[indx] - fee + transact(indx+1 , True)  
+            dp[(indx , can )] = max(op1 , op2)
+            return dp[(indx , can)]
             
         
         
