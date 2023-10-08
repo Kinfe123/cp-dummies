2522be8014c9cb326be1dd5cdc80c0a9dc11383e
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-10-08 08:09:50 +0300

    Time: 5 ms (40.62%), Space: 8 MB (16.81%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -19,10 +19,29 @@ long f(int r , int c , vector<vector<long>>&dp ,  vector<vector<int>> &mat){
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-         
-        vector<vector<long>> dp(n , vector<long>(m , -1));
-    // dp[0][0] = 1;
-    return f(n-1 , m-1 , dp , obstacleGrid) % mod;
+         // dp[i] = curr - dp[i-1] = prev
+        vector<int> prev(m , 0);
+        for(int i = 0; i < n; i++){
+            vector<int> curr(m , 0);
+            for(int j = 0; j<m; j++){
+                if(obstacleGrid[i][j] == 1) curr[j] = 0;
+                else if(i == 0 && j == 0) curr[j] = 1;
+                else{
+                int up = 0 , left =0 ;
+                    if(i >0){
+                        up = prev[j];
+                    }if(j > 0){
+                        left = curr[j-1];
+                    }
+                curr[j] = (up + left) % mod;
+                }
+                
+            }
+            prev = curr;
+            
+
+        }
+        return prev[m-1];
         
     }
 };
