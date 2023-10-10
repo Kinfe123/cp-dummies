f37c37a57de4ab741f20ce2e7dc3bc687af505c0
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-10-10 16:09:24 +0300

    Time: 127 ms (34.19%), Space: 104.9 MB (96.81%) - LeetHub

--- 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.cpp
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.cpp
@@ -1,39 +1,12 @@
 class Solution {
 public:
     int maxSubarrays(vector<int>& nums) {
-        if(nums.size() == 1){
-            return 1;
-        }
-        int curr = nums[0];
-        int n = nums.size();
-        
-        int target = nums[0];
-        for(int i = 1; i<n; i++){
-            target = target & nums[i];
-        }
-        
-        int count = 1;
-        // if(curr == 0){
-        //     count += 1;
-        // }
-        for(int i=1; i<n; i++){
-            if(curr == 0){
-                count += 1;
-                curr = nums[i];
-            }else{
-                curr &= nums[i];
-            }
-            // curr &= nums[i];
-            // cout << "The current score: " << curr <<endl;
-            
-                
-            }
-        if(curr > 0 && count > 1){
-            count--;
-        }
-        
-            
-    
-        return count;
+    int res = 0, cur = 0;
+    for (int n : nums) {
+        cur = cur == 0 ? n : cur & n;
+        res += cur == 0;
+    }
+    return max(1, res);
+
     }
 };
