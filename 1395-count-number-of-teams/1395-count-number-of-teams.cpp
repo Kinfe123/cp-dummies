40627328bc274f96b7e9461517eecae176a16419
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-10-11 15:37:04 +0300

    Time: 64 ms (87.32%), Space: 10.8 MB (26.95%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,37 +1,28 @@
+#include <vector>
+
 class Solution {
 public:
-    int numTeams(vector<int>& rating) {
+    int numTeams(std::vector<int>& rating) {
         int n = rating.size();
-        int count = 0;
-
-        for(int i=0; i<n; i++) {
-
-            int ls=0, ll=0, rs=0, rl=0;
-
-            for(int l=i-1; l>=0; l--)  {
-                if(rating[l]<rating[i])   {
-                    ls++;
-                }
-                else {
-                    ll++;
-                }
-            }
-
-            for(int r=i+1; r<n; r++)  {
-                if(rating[r]<rating[i])   {
-                    rs++;
-                }
-                else {
-                    rl++;
-                }
-            }
-
-
-            count += (ls*rl) + (ll*rs);
-
+        std::vector<std::vector<int>> store(n, std::vector<int>(2, 0)); // Greater, Smaller
+        int answer = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int right = rating[i];
             
+            for (int j = i - 1; j >= 0; j--) {
+                int left = rating[j];
+                
+                if (right > left) {
+                    store[i][0] += 1;
+                    answer += store[j][0];
+                } else if (right < left) {
+                    store[i][1] += 1;
+                    answer += store[j][1];
+                }
+            }
         }
-
-        return count;
+        
+        return answer;
     }
 };
