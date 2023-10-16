9a0c3b5b065b418afd01da83ec1cac63a267a3ef
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-10-16 09:51:10 +0300

    Time: 4 ms (5.17%), Space: 6.9 MB (8.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+    
+    int count(int r , int c , vector<vector<int>>&dp){
+        if(dp[r][c] != -1) return dp[r][c];
+        
+        if(r == 0 || c == 0) return 1;
+        if(r < 0 || c < 0) return 0;
+        int up = count(r-1 ,c , dp);
+        int left = count(r , c-1 , dp);
+        dp[r][c]= up + left;
+        return dp[r][c] ;
+    }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m , vector<int>(n , -1));
-        dp[0][0]=1;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i == 0 && j == 0) continue;
-                else if(i -1 < 0 || j - 1 < 0) continue;
-                else{
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
-        }
+        // dp[0][0] = 1;
+        return count(m -1, n-1 , dp);
+//         for(int i=1; i<m; i++){
+//             for(int j = 1; j<n; j++){
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
+//             }
+//         }
         
-        if(dp[m-1][n-1] < 0) return -1 * dp[m-1][n-1];
-        else {
-            
-         return dp[m-1][n-1];   
-        }
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 cout<< "The values are: " << dp[i][j] << endl;
+//             }
+//         }
+        // return dp[m-1][n-1];
+        
     }
 };
