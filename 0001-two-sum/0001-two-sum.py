9b6b818726fba6dbc4229cb454f04e482b296c6d
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-11-15 22:38:33 +0300

    Time: 70 ms (48.92%), Space: 17.9 MB (5.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map_ = defaultdict(int)
         res = []
-        
+        map_ = defaultdict(int)
         for i in range(len(nums)):
-            
+            looking_for = target - nums[i]
             if nums[i] in map_:
-                res = [map_[nums[i]] , i]
-                # return [i , map_[target - nums[i]]]
-                break
+                return [map_[nums[i]] ,  i]
+            else:
+                map_[looking_for] = i
+                
+        return [-1 , -1]
+                
             
-            
-            map_[target-nums[i]] = i
-            # print('THe mapped ; ', map_)
-        return res
         
+                    
