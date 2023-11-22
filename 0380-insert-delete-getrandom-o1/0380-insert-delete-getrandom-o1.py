5a49916375be4f27f47447ae9a4a917616e5120a
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-11-22 22:48:08 +0300

    Time: 295 ms (95.03%), Space: 63.7 MB (27.95%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,35 +2,40 @@ class RandomizedSet:
 
     def __init__(self):
         self.array = []
-        self.map = defaultdict(int)
+        self.mapper = defaultdict(int)
+        
 
     def insert(self, val: int) -> bool:
-        if val in self.map:
+        if val in self.mapper:
             return False
         else:
-            self.map[val] = len(self.array)
+            self.mapper[val] = len(self.array)
+            #adjusting the indexing issue
             self.array.append(val)
+            return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.mapper:
+            return False
+        else:
+            last_ele = self.array[-1]
+            rem_idx = self.mapper[val]
+            last_idx = self.mapper[self.array[-1]]
+            self.mapper[self.array[-1]] = rem_idx
+            self.mapper[val] = last_idx
+            self.array[rem_idx] = last_ele
+            self.array[last_idx] = val
+            self.array.pop()
+            self.mapper.pop(val)
+            
+            
             return True
         
 
-    def remove(self, val: int) -> bool:
-        if val not in self.map:
-            return False
-        else:
-            removed_idx = self.map[val]
-            lst_ele = self.array[-1]
-            self.array[removed_idx] = lst_ele
-            self.map[lst_ele] = removed_idx
-            # self.map[val]
-            self.array[-1] = val
-            self.array.pop()
-            self.map.pop(val)
-            
-            return True
-            
-
     def getRandom(self) -> int:
         return random.choice(self.array)
+        
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
